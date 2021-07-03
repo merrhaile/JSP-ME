c17930c05d803455fe41c23145035300b37d181b
Meron Eyeram <35456982+merrhaile@users.noreply.github.com>
2021-07-03 16:17:24 -0400

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,8 @@
+import * as Maths from './Maths';
+
+console.log(Maths.add(2, 2))
+console.log(Maths.subtract(2, 2))
+console.log(Maths.multiply(2, 2))
+console.log(Maths.divide(2, 2))
+console.log(Maths.PI)
+

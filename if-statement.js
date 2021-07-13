2f5a24e18543cc901d0cd5496b77cfd9abf39b1f
meroneyeram <merrhaile@gmail.com>
2021-07-13 09:38:54 -0400

    Created if-statement.js

---
+++ if-statement.js
@@ -0,0 +1,8 @@
+// check whether a given number is even or odd 
+let num = 4;
+
+if (num % 2 == 0) {               // if the remiander of a number  
+    console.log('the number is even');  // divided by 2 is 0, the number 
+} else {                          // is even else it is odd
+    console.log("the number is odd");
+}

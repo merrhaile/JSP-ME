b521526f219fb5fce2ef3d9e76d82cedb146b474
meroneyeram <merrhaile@gmail.com>
2021-07-14 17:33:05 -0400

    Created while-loop.js

---
+++ while-loop.js
@@ -0,0 +1,8 @@
+
+let result = 1;
+let num = 5;
+while(num > 0){
+    result *= num;   // equivalent to (result = result * num)
+    num--;      // equivalent to (num = num - 1)
+}
+console.log(result)

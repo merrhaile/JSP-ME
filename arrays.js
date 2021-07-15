d73819f3a33d2acd57e6660f795a1d116e091a94
meroneyeram <merrhaile@gmail.com>
2021-07-15 15:24:23 -0400

    Created arrays.js

---
+++ arrays.js
@@ -0,0 +1,7 @@
+// In javascript an array is a group of elements 
+// separated by commas and enclosed in a bracket
+
+//Declaring and defining an array
+let arr = [];    // an empty array
+let arr1 = [1,2,3,4,5];  //an array of numbers
+let arr2 = ['one', 'two', 'three'];  // an array of strings

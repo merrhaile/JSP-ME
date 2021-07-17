a381e6e6ec5776e8ae7295287d0858212150388e
Meron Eyeram <35456982+merrhaile@users.noreply.github.com>
2021-07-17 16:21:35 -0400

    Create object.js

---
+++ object.js
@@ -0,0 +1,13 @@
+// Objects in JavaScript, just as in many other programming languages, 
+// can be compared to objects in real life. In JavaScript, an object 
+// is a standalone entity, with properties and type. 
+
+// Declaring and defining an object
+
+const car = {
+    make: "Toyota",
+    model: "Camery",
+    year: "2014",
+};
+
+console.log(car);

305ef27762a4aa003e1ed09ceb4378405f85d2ad
Meron Eyeram <35456982+merrhaile@users.noreply.github.com>
2021-07-20 16:21:56 -0400

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,10 @@
+// A function in JavaScript is a block of code designed to perform a particular task. 
+// it takes some input and return an output where there is some obvious relationship 
+// between the input and the output. A JavaScript function is executed when "something" 
+// invokes it (calls it).
+
+function addNum(n1, n2) {
+  return n1 + n2;
+}
+
+console.log(addNum(4, 5));

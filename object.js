7430b758857a7db4b26dfb9394dd8fac8848006f
meroneyeram <merrhaile@gmail.com>
2021-09-06 23:13:46 -0500

    Updated objects

--- object.js
+++ object.js
@@ -4,10 +4,38 @@
 
 // Declaring and defining an object
 
-const car = {
-    make: "Nissan",
+// objects can be created in two ways. {...} is object litral
+let car = {};
+let car1 = new Object();
+
+// object litrals and properties
+const cars = {
+    make: "Nissan",  // propreties are entered in form of key:value
     model: "Rogue",
     year: "2019",
 };
+// to add a property
+console.log(cars);
+cars.vin = 234; // property value can be added using dot notation
+console.log(cars);
 
-console.log(car);
+// to delete a property
+delete cars.vin;
+console.log(cars);
+
+//expressions can be applied within the bracket notation
+let vin = "vin"
+cars[vin+"Number"] = 254;
+
+console.log(cars);
+console.log(cars.make); // accessing a property value using dot
+console.log(cars.year); 
+
+// the "for...in" loop is a special form of loop that acesses the property names
+// instead of values
+for (let property in cars){
+    console.log(property); // returns the property names
+}
+for (let property in cars){
+    console.log(cars[property]); // returns the property values
+}
